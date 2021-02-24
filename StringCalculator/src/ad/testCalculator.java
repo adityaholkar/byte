a0456cdddf3c9f9184369d06c6037e1c746bd983
aditya <aditya.holkar17@vit.edu>
2021-02-24 23:40:59 +0530

    writing test case for single valued test case

--- StringCalculator/src/ad/testCalculator.java
+++ StringCalculator/src/ad/testCalculator.java
@@ -1,12 +1,24 @@
 package ad;
-
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 public class testCalculator {
+	private Calculator calculator ;
+	@BeforeTest
+	public void setup() {
+		calculator = new Calculator();
+	}
+	
+		
 	
 	@Test
 	public void emptyStringAnswerNull() {
-		Calculator calculator = new Calculator();
+		
 		assertEquals(calculator.add(""), 0);
+		
+	}
+	@Test
+	public void singleValuedAnswer() {
+		assertEquals(calculator.add("1"),1);
 	}
 }
