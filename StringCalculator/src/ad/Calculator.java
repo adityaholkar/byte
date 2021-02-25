ad8a09a0d5bf173bfd35dbb74af3a08ac7ac53db
aditya <aditya.holkar17@vit.edu>
2021-02-25 11:51:10 +0530

    adding delimiter ,

--- StringCalculator/src/ad/Calculator.java
+++ StringCalculator/src/ad/Calculator.java
@@ -1,12 +1,16 @@
 package ad;
 
 public class Calculator {
-
+	 
 	public int add(String input) {
+		String[] numbers = input.split(",");
 		if(isEmpty(input)) {
 			return 0;
 		}
-		return strToInt(input);
+		else {
+			return strToInt(numbers[0])+strToInt(numbers[1]);
+		}
+		
 	}
 	private boolean isEmpty(String input) {
 		return input.isEmpty();
