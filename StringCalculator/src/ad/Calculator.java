6a04a0039c423ae4758fa7d1aec753105bfcad98
aditya <aditya.holkar17@vit.edu>
2021-02-25 19:12:16 +0530

    refactoring after delimeter change update

--- StringCalculator/src/ad/Calculator.java
+++ StringCalculator/src/ad/Calculator.java
@@ -2,39 +2,33 @@ package ad;
 
 public class Calculator {
 	 private static String delimeter = ",|\n";
-	public int add(String input) {
+	 public int add(String input) {
 		
 		if(isEmpty(input)) {
 			return 0;
 		}
-		if(input.length() == 1) {
-			return strToInt(input);
-		}
 		else {
 			if(input.matches("//(.*)\n(.*)")) {
-				String temp = Character.toString(input.charAt(2));
-				delimeter=delimeter+"|"+temp;
+				delimeter = delimeter+"|"+ Character.toString(input.charAt(2));
 				input = input.substring(4);
 			}
-			
-			
 			String[] numbers = input.split(delimeter);
 			return getSum(numbers);
 		}
 		
-	}
-	private int getSum(String[] numbers) {
+	 }
+	 private int getSum(String[] numbers) {
 		
 		int sum=0;
 		for(String number:numbers) {
 			sum+=strToInt(number);
 		}
 		return sum;
-	}
-	private boolean isEmpty(String input) {
+	 }
+	 private boolean isEmpty(String input) {
 		return input.isEmpty();
-	}
-	private int strToInt(String input) {
+	 }
+	 private int strToInt(String input) {
 		return Integer.parseInt(input);
-	}
+	 }
 }
